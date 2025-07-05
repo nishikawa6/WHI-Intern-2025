0742357c5a9d270182dc4adf15a3fa81d6db79bf
tofu <66718374+toufu-cell@users.noreply.github.com>
2025-07-05 17:07:40 +0900

    attributerefinement

--- frontend/src/models/Employee.ts
+++ frontend/src/models/Employee.ts
@@ -4,6 +4,10 @@ export const EmployeeT = t.type({
   id: t.string,
   name: t.string,
   age: t.number,
+  department: t.string,
+  position: t.string,
+  email: t.string,
+  skills: t.array(t.string),
 });
 
 export type Employee = t.TypeOf<typeof EmployeeT>;
