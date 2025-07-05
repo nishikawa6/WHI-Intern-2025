c622bb129e5cf441c91c417a96465b4573aeb962
tofu <66718374+toufu-cell@users.noreply.github.com>
2025-07-06 07:59:43 +0900

    Update EmployeeDatabaseDynamoDB.ts

--- backend-ts/src/employee/EmployeeDatabaseDynamoDB.ts
+++ backend-ts/src/employee/EmployeeDatabaseDynamoDB.ts
@@ -55,7 +55,11 @@ export class EmployeeDatabaseDynamoDB implements EmployeeDatabase {
       return [];
     }
     return items
-      .filter((item) => filterText === "" || item["name"].S === filterText)
+      .filter((item) => {
+        const name = (item["name"]?.S ?? "").toLowerCase();
+        const keyword = filterText.toLowerCase();
+        return keyword === "" || name.includes(keyword);
+      })
       .map((item) => {
         return {
           id: item["id"].S,
@@ -75,41 +79,9 @@ export class EmployeeDatabaseDynamoDB implements EmployeeDatabase {
         } else {
           return [decoded.right];
         }
+      });
   }
-
-    async getEmployees(filterText: string): Promise<Employee[]> {
-        const input: ScanCommandInput  = {
-            TableName: this.tableName,
-        };
-        const output = await this.client.send(new ScanCommand(input));
-        const items = output.Items;
-        if (items == null) {
-            return [];
-        }
-        return items
-            .filter(item => {
-                const name = (item["name"]?.S ?? "").toLowerCase();
-                const keyword = filterText.toLowerCase();
-                return keyword === "" || name.includes(keyword);
-            })
-            .map(item => {
-                return {
-                    id: item["id"].S,
-                    name: item["name"].S,
-                    age: mapNullable(item["age"].N, value => parseInt(value, 10)),
-                }
-            }).flatMap(employee => {
-                const decoded = EmployeeT.decode(employee);
-                if (isLeft(decoded)) {
-                    console.error(`Employee ${employee.id} is missing some fields and skipped. ${JSON.stringify(employee)}`);
-                    return [];
-                } else {
-                    return [decoded.right];
-                }
-            });
-    }
 }
-
 function mapNullable<T, U>(
   value: T | null | undefined,
   mapper: (value: T) => U
